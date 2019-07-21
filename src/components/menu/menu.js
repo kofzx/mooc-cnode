d00b32f66cfc7f0d412aebea875cb920df5e2549
kofzx <2670476383@qq.com>
2019-07-21 23:30:15 +0800

    add menu reducer

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,14 +1,25 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text, Button, Image } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
+import { showDrawer } from '../../actions/menu'
 import './menu.less'
 
+@connect(function (store) {
+  return {...store.menu}
+}, function (dispatch) {
+  return {showMenu() {
+    dispatch(showDrawer())
+  }}
+})
 class Menu extends Component {
+    showDrawer() {
+      this.props.showMenu && this.props.showMenu();
+    }
     render () {
         return (
             <View className='topiclist-menu'>
-                <Image className='image' src={require('../../assets/images/menu.png')}></Image>
-                <Text>全部</Text>
+                <Image className='image' src={require('../../assets/images/menu.png')} onClick={this.showDrawer.bind(this)}></Image>
+                <Text>{this.props.currentCata ? this.props.currentCata.value : ''}</Text>
                 <Image className='image' src={require('../../assets/images/user.png')}></Image>
             </View>
         )
